9d5f032f1044a3b03c7a285ef2574aeb5ac8d248
Irosha Dhananjaya <irodhananjaya123@gmail.com>
2026-08-23 18:51:38 +0530

    c update

--- MyWeb/script.js
+++ MyWeb/script.js
@@ -2,3 +2,16 @@ document.getElementById("name_button").onclick = function()
 {
     onclick = window.alert("Submitted Successfully");
 }
+
+function updateClock(){
+
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
