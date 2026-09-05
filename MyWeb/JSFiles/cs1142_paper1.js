c1f89ba0cda5b1ebd116d0a4048e29d7c1dd7140
Irosha Dhananjaya <irodhananjaya123@gmail.com>
2026-09-06 00:02:04 +0530

    Add Marking system

--- MyWeb/JSFiles/cs1142_paper1.js
+++ MyWeb/JSFiles/cs1142_paper1.js
@@ -12,6 +12,59 @@ function updateTimer() {
         timerElement.style.color = "red";
     }
 }
+
+function checkAnswers() {
+    let score = 0;
+    let totalQuestions = 6;
+    let answers = [1,2,3,4,1,2];
+    let question4Answers = ["true","False","True","True"];
+    let question6Answers = [1,3,4];
+    
+
+    // Check each question's answer
+
+    if(totalQuestions <= 6){
+        for(let i=1; i<=6; i++){
+            let questionContainer = document.getElementById(`question${i}`);
+            if(i === 4){ // choose question 4 and 6
+                for(let j=1; j<=4; j++){
+                    let boolValue = document.querySelector(`input[name="question4_answer${j}"]:checked`)?.value;
+                    if(boolValue === question4Answers[j-1]){
+                        score++;
+                        questionContainer.style.background = "linear-gradient(to left, white,rgb(0, 255, 0))";
+                    }
+                    else{
+                        questionContainer.style.background = "linear-gradient(to left, white,rgb(255, 2, 2))";
+                    }
+                    
+                }
+            }
+            else if(i == 6){
+                let question6Value = document.querySelectorAll(`input[name = "question6"]:checked`);
+                if(question6Value.length === question6Answers.length && Array.from(question6Value).every(input => question6Answers.includes(parseInt(input.value)))){
+                    score++;
+                    questionContainer.style.background = "linear-gradient(to left, white,rgb(0, 255, 0))";
+                }
+                else{
+                    questionContainer.style.background = "linear-gradient(to left, white,rgb(255, 2, 2))";
+                }
+            }
+            else{
+                let value = document.querySelector(`input[name="question${i}"]:checked`)?.value;
+                if(value == answers[i-1]){
+                    score++;
+                    questionContainer.style.background = "linear-gradient(to left, white,rgb(0, 255, 0))";
+                }
+                else{
+                    questionContainer.style.background = "linear-gradient(to left, white,rgb(255, 2, 2))";
+                }
+            }
+            
+        }
+    }
+    document.getElementById("marks").textContent = `Your score: ${score} out of ${totalQuestions}`;
+    console.log(`Final score: ${score}`);
+}
 //updateTimer(); // Initial call to display the timer immediately
 
 let timerInterval = setInterval(() => {
