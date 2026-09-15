78ba535c77bee23c9da455097c2089d3ca37917d
Irosha Dhananjaya <irodhananjaya123@gmail.com>
2026-09-15 22:53:45 +0530

    Update submission form

--- docs/JSFiles/personal_form.js
+++ docs/JSFiles/personal_form.js
@@ -1,4 +1,14 @@
 let submitButton = document.getElementById("submit_button");
+let form = document.getElementById("form");
+
+form.addEventListener("submit", function (event){
+    if(form.checkValidity()){
+        Hide();
+    }
+    else{
+        form.reportValidity();
+    }
+});
 
 function Hide(){
     submitButton.style.display = "none";
