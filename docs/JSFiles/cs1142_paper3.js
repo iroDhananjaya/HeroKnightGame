119ce087c6d08a736b0a8727fe603445a0c78a5e
Dineth Pansilu <dinethpansilu2004@gmail.com>
2026-09-17 18:31:02 +0530

    Mark System Updated

--- docs/JSFiles/cs1142_paper3.js
+++ docs/JSFiles/cs1142_paper3.js
@@ -1,17 +1,75 @@
-let CorerctAnswers = [ '2', '3', '2', '4', '3', '2', '4', '1', '2', '4' ];
+let CorrectAnswers = "2,3,2,4,3,2,4,1,2,4".split(',');
 
-console.log(CorerctAnswers);
+console.log(CorrectAnswers);
 
 document.getElementById('submit').onclick = function() {
     event.preventDefault();
 
-    let answers = document.querySelectorAll('input[type="radio"]:checked');
+    document.querySelectorAll(`input[type="radio"]`).forEach(radio => {
+        radio.disabled =true;
+    });
 
-    answers = Array.from(answers).map(answers => answers.value);
+    // Answers Get to Array
+    let answers = [];
+    for (let i=0; i<CorrectAnswers.length; i++) {
+        
+        let checkInput = document.querySelector(`input[name="q${i+1}"]:checked`);
+        
+        if (checkInput) {
+            answers.push(checkInput.value);
+        } else {
+            answers.push(null);
+        }
+    }
 
-    let x=2;
+    console.log(answers);
 
-    console.log(x);
+    //Alert
+    /* if (answers.length < CorrectAnswers.length) {
+        alert("සියලුම ප්‍රශ්න වලට උත්තර දෙන්න!");
+        return;
+    }; */
+
+    // Check Answers
+    let total = 0;
+    for (i = 0; i < CorrectAnswers.length; i++) {
+        if (answers[i] == CorrectAnswers[i]) {
+            total += 1;
+        } else {
+            event.preventDefault();
+
+            // Wrong Answer Highlight Red
+            let radio = document.querySelector(`input[name="q${i+1}"][value="${answers[i]}"]`);
+            
+
+            if (radio) {
+            radio.parentElement.style.backgroundColor = "red";
+            radio.parentElement.style.border = "2px solid #30dfef"
+            radio.style.accentColor = "blue";
+            };
+
+            // Correct Answer Highlight Green
+            let radiocorrect = document.querySelector(`input[name="q${i+1}"][value="${CorrectAnswers[i]}"]`);
+
+
+            if (radiocorrect) {
+            radiocorrect.parentElement.style.backgroundColor = "#35F527";
+            radiocorrect.style.accentColor = "blue";
+            }
+
+        }
+    };  
+    console.log(total);
+
+    document.getElementById("mark").innerHTML = total;
+    document.getElementById("markbox").style.display = "flex";
+
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+
+
 
 };
 
